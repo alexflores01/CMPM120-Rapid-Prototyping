1ce1be5a5f49d2815739c4ec9d1a00c9dadbe9e5
alexflores <flores7alex@gmail.com>
2026-05-21 23:51:59 -0700

    disable physics debug mode

--- coreGameplay.js
+++ coreGameplay.js
@@ -184,7 +184,7 @@ const config = {
         default: 'arcade',
         arcade: {
             gravity: {y: 300},
-            debug: true,
+            debug: false,
         },
     },
     scene: [Title, Gameplay]
